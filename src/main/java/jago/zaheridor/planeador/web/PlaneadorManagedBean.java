1f61aaefed2d53777f3defecc33069c4d74c0d83
jago1287 <jago1287@gmail.com>
2018-04-13 12:10:37 -0500

    T01: Carga de un archivo desde una ruta fija. Task #1 - Leer un archivo CSV de una ruta fija

---
+++ src/main/java/jago/zaheridor/planeador/web/PlaneadorManagedBean.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jago.zaheridor.planeador.web;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+
+/**
+ *
+ * @author zaheridor
+ */
+@Named(value = "planeadorManagedBean")
+@RequestScoped
+public class PlaneadorManagedBean {
+    
+    private static final Logger logger = Logger.getLogger("jago.zaheridor.planeador.web.PlaneadorManagedBean");
+    private String mensaje;
+    private final String ARCHIVO = "/Users/zaheridor/Documents/STUDY/proyecto_planeador/planeador/src/main/resources/archivo.csv";
+
+    /**
+     * Creates a new instance of PlaneadorManagedBean
+     */
+    public PlaneadorManagedBean() {
+        setMensaje("Primera carga");
+    }
+    
+    /**
+     * Acción del botón de cargar archivo.
+     * @return
+     * @throws IOException 
+     */
+    public String cargar(){
+        
+        try (BufferedReader br = new BufferedReader(new FileReader(ARCHIVO)))
+        {
+            String line;
+		
+            while ((line = br.readLine()) != null) {
+                logger.log(Level.INFO, "ruta: "+line);
+            }
+
+	} catch (IOException e) {
+            logger.warning(e.getMessage());
+	}
+        
+        return "";
+    }
+    
+    public String getMensaje() {
+        return mensaje;
+    }
+
+    public void setMensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+
+}
